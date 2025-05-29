6ca3cdf8592c46f700516599b5265a94c47c091e
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-05-28 23:10:03 -0500

    render

---
+++ fraction-game/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FractionGame from "./components/FractionGame";
+import "./App.css";
+import "./styles/styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Fraction Game!</h1>
+      <FractionGame />
+    </div>
+  );
+}
+
+export default App;
